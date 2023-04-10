2c84e242376c1895d9c209dd65a3811fd1590fb2
OmerMasih <you@example.com>
2023-04-09 22:32:34 -0700

    Creating my server.js, fixing bugs caused errors remodifying my develop folder and accessing my files

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,13 +6,14 @@ class Category extends Model {}
 
 Category.init(
   {
+    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
     },
-    Category_name: {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
